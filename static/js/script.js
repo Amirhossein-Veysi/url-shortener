e9df592ed301ff50fd313b724c327370b029c38a
Amirhossein Veysi <amirhossein_veysi@yahoo.com>
2021-08-12 17:41:25 +0430

    Updated

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,38 @@
 //! Variable
 
-let dElNvBrClps = $('.navbar-collapse-sm');
+let scrlSmSiz = 567;
 
 //! DOM Event
 
 $(window).on('resize', deNvBrClpsHidHandler);
 
+$('.next-btn').on('click', deUrlInpMovrToLft);
+
+$('.previous-btn').on('click', deUrlInpMovrToRigt);
+
 //! DOM Event Handler
 
 function deNvBrClpsHidHandler(){
-    if($(window).width() >= 567){
+    if($(window).width() >= scrlSmSiz){
         $('.navbar-collapse-sm').attr('style', 'display: none !important;');
     }else{
         $('.navbar-collapse-sm').attr('style', '');
     }
 }
 
+function deUrlInpMovrToLft(){
+    $('.url-input-container').css('left', '-200%');
+    $('.password-input-container').css('right', '0');
+}
+
+function deUrlInpMovrToRigt(){
+    $('.url-input-container').css('left', '0');
+    $('.password-input-container').css('right', '-200%');
+}
+
 //! Aditinal Functions
+
+
 
 //! Aditinal Call
 
